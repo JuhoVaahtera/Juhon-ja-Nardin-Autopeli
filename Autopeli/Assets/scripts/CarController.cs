436900fe1b93f214dba13e5cc962058119022f0e
JuhoVaahtera <juhovaahtera05@gmail.com>
2023-02-10 14:27:52 +0200

    s
    s

--- Autopeli/Assets/scripts/CarController.cs
+++ Autopeli/Assets/scripts/CarController.cs
@@ -16,6 +16,8 @@ public class CarController : MonoBehaviour
     {
         public GameObject wheelModel;
         public WheelCollider wheelCollider;
+        public GameObject wheelEffectObj;
+        public ParticleSystem smokeParticle;
         public Axel axel;
     }
 
@@ -34,16 +36,21 @@ public class CarController : MonoBehaviour
 
     private Rigidbody carRb;
 
+    private CarLights carLights;
+
     private void Start()
     {
         carRb = GetComponent<Rigidbody>();
         carRb.centerOfMass = _centerOfMass;
+
+        carLights = GetComponent<CarLights>();
     }
 
     void Update()
     {
         GetInputs();
         AnimatedWheels();
+        WheelEffects();
     }
     void LateUpdate()
     {
@@ -80,12 +87,15 @@ public class CarController : MonoBehaviour
 
     void Brake()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || moveInput == 0)
         {
             foreach(var wheel in wheels)
             {
                 wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
             }
+
+            carLights.isBackLightOn = true;
+            carLights.OperateBackLights();
         }
         else
         {
@@ -93,6 +103,9 @@ public class CarController : MonoBehaviour
             {
                 wheel.wheelCollider.brakeTorque = 0;
             }
+
+            carLights.isBackLightOn = false;
+            carLights.OperateBackLights();
         }
     }
 
@@ -105,6 +118,24 @@ public class CarController : MonoBehaviour
             wheel.wheelCollider.GetWorldPose(out pos, out rot);
             wheel.wheelModel.transform.position = pos;
             wheel.wheelModel.transform.rotation = rot;
+        }
+    }
+
+    void WheelEffects()
+    {
+        foreach (var wheel in wheels)
+        {
+            //var dirtParticleMainSettings = wheel.smokeParticle.main;
+
+            if (Input.GetKey(KeyCode.Space) && wheel.axel == Axel.Rear)
+            {
+                wheel.wheelEffectObj.GetComponentInChildren<TrailRenderer>().emitting = true;
+                
+            }
+            else
+            {
+                wheel.wheelEffectObj.GetComponentInChildren<TrailRenderer>().emitting = false;
+            }
         }
     }
 }
