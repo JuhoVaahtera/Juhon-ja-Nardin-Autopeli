dd0ad6b4dabafa21ab57d139e299a41652179033
JuhoVaahtera <juhovaahtera05@gmail.com>
2023-02-16 10:53:31 +0200

    s
    s

--- Autopeli/Assets/scripts/ScoreScript.cs
+++ Autopeli/Assets/scripts/ScoreScript.cs
@@ -10,6 +10,8 @@ public class ScoreScript : MonoBehaviour
     private FloatSO ScoreSO;
     public static int scoreValue = 0;
     Text score;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,16 @@ public class ScoreScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if(timer > 5f)
+        {
+            ScoreSO.Value += 5;
+
+            timer = 0;
+        }
+
+
         score.text = "Score: " + ScoreSO.Value;
     }
 }
